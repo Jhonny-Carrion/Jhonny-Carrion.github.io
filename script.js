e2da45cc659d82446f8bbc293f7a876bda5be19d
Jhonny-Carrion <ingindustrialcarrion@gmail.com>
2024-10-28 21:12:52 +0100

    update

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
         $('#contactForm').slideToggle();
     });
 
-    // Smooth scroll to sections when clicking on links (if added)
-    $('a[href^="#"]').on('click', function(event) {
+    // Smooth scroll to sections when clicking on navigation links
+    $('nav a').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
-                scrollTop: target.offset().top
+                scrollTop: target.offset().top - 20 // Offset for header height
             }, 1000);
         }
     });
